f04e48ddf4f92646a2a972064e2e4716cfcf684b
Yann Frendo <yann.frendo@live.fr>
2021-04-20 11:50:22 +0200

    Mise en forme

--- json_to_jpeg.py
+++ json_to_jpeg.py
@@ -1,53 +1,94 @@
 import os
-
 from PIL import Image, ImageDraw, ImageFont
 import pandas as pd
 import sys
+from tqdm import tqdm #Une petite barre de progression on sait jamais ca peut etre long
 
 COLOR = ['red','blue','green'] #Couleurs pour nos segmentations
-#path_json = sys.argv[0]
-#path_image = sys.argv[1]
-path_json = "/home/yfrendo/Data/DATASET MorphoTique/0-Ixodes_masques/test_json.json"
-path_image = "/home/yfrendo/Data/DATASET MorphoTique/0-Ixodes"
+
+path_json = str(sys.argv[1]) #En premier le chemin vers le JSON
+path_image = str(sys.argv[2]) #En deuxième vers les images
+
+#path_json = "/home/yfrendo/Data/DATASET MorphoTique/0-Ixodes_masques/anotation.json"
+#path_image = "/home/yfrendo/Data/DATASET MorphoTique/0-Ixodes/"
+#test
+
+reshape_width = 512 #Taille pour le reshape
+reshape_height = 512
+
 df_json = pd.read_json(path_json)
 df_json = df_json.transpose().reset_index()[['filename','regions']] #On réduit un peu
 
-if not os.path.exists('Masques'):
+if not os.path.exists('Masques'): #On fait les dossier de sauvegarde des sorties
     os.makedirs('Masques')
+if not os.path.exists('Images'):
+    os.makedirs('Images')
 
-for image_id in range(len(df_json['regions'])):
-    image = path_image + '/' + df_json['filename'][image_id]
+for image_id in tqdm(range(len(df_json['regions']))):
+    image = path_image  + df_json['filename'][image_id]
     im = Image.open(image)
     x, y = im.size #Tille de l'image
-    font = Image.new('RGB',(x,y),(0, 0, 0)) #Un fond blanc de la meme taille que les images
+    font = Image.new('RGB',(x,y),(0, 0, 0)) #Un fond noir de la meme taille que les images
     draw = ImageDraw.Draw(font) #Pour dessiner nos polygones
 
     all_poly = [] #Pour ranger tout nos polygones
     for segmentation in range(len(df_json['regions'][image_id])):
-        all_x = df_json['regions'][image_id][segmentation]['shape_attributes']['all_points_x'] #Extraction des X
-        all_y = df_json['regions'][image_id][segmentation]['shape_attributes']['all_points_y'] #Extraction des Y
 
-        poly = [] #Création de la liste pour créer le polygone
-
-        if len(all_x) != len(all_y): #Une petite vérification
-            raise ValueError('Il doit y avoir autant de X que de Y dans le polygone')
-
-        for k in range(len(all_x)):
-            poly.append((all_x[k],all_y[k])) #Création des tupples pour le polynome
+        try: #On vérifie que tout est en place
+            df_json['regions'][image_id][segmentation]['region_attributes']['Tique']
+            pass
+        except:
+            raise ValueError('Il doit y avoir un attribut Tique')
         
-        if list(df_json['regions'][image_id][segmentation]['region_attributes']['Region'].keys())[0] == "Bouclier": #On détermine la couleur en fonction de la segmentation
-            col = COLOR[0]
+        try:
+            df_json['regions'][image_id][segmentation]['region_attributes']['Region']
+            pass
+        except:
+            raise ValueError('Il doit y avoir un attribut Region')
+
+        if df_json['regions'][image_id][segmentation]['region_attributes']['Tique'] == "non":
+            all_x = df_json['regions'][image_id][segmentation]['shape_attributes']['all_points_x'] #Extraction des X
+            all_y = df_json['regions'][image_id][segmentation]['shape_attributes']['all_points_y'] #Extraction des Y
+
+            poly = [] #Création de la liste pour créer le polygone
+
+            if len(all_x) != len(all_y): #Une petite vérification
+                raise ValueError('Il doit y avoir autant de X que de Y dans le polygone')
+
+            for k in range(len(all_x)):
+                poly.append((all_x[k],all_y[k])) #Création des tupples pour le polynome
+            
+            if list(df_json['regions'][image_id][segmentation]['region_attributes']['Region'].keys())[0] == "Bouclier": #On détermine la couleur en fonction de la segmentation
+                col = COLOR[0]
+            
+            elif list(df_json['regions'][image_id][segmentation]['region_attributes']['Region'].keys())[0] == "Abdomen":
+                col = COLOR[1]
+
+            elif list(df_json['regions'][image_id][segmentation]['region_attributes']['Region'].keys())[0] == "Rostre":
+                col = COLOR[2]
+            else:
+                raise ValueError("Les segmentations doivent etre : Bouclier, Abdomen ou Rostre \nValeur de la segmentation = " 
+                + str(df_json['regions'][image_id][segmentation]['region_attributes']['Region'].keys())[0]) #Au cas ou quelqun se trompe sur la segmentation
+
+            all_poly.append((poly,col))
         
-        elif list(df_json['regions'][image_id][segmentation]['region_attributes']['Region'].keys())[0] == "Abdomen":
-            col = COLOR[1]
+        else: #Détection de la zone de l'intéret et crop
+            x_crop = df_json['regions'][image_id][segmentation]['shape_attributes']['x']
+            y_crop = df_json['regions'][image_id][segmentation]['shape_attributes']['y']
+            width_crop = df_json['regions'][image_id][segmentation]['shape_attributes']['width']
+            height_crop = df_json['regions'][image_id][segmentation]['shape_attributes']['height']
 
-        elif list(df_json['regions'][image_id][segmentation]['region_attributes']['Region'].keys())[0] == "Rostre":
-            col = COLOR[2]
-
-        all_poly.append((poly,col))
     
     for polygone,couleur in all_poly:
         draw.polygon(xy = polygone,fill= couleur)
-    font.save("./Masques/" + df_json['filename'][image_id] + "_masque", "JPEG")
+
+    im = im.crop((x_crop,y_crop,x_crop + width_crop, y_crop + height_crop)) #Découpe zone interet
+    font = font.crop((x_crop,y_crop,x_crop + width_crop, y_crop + height_crop))
+
+    im = im.resize((reshape_width,reshape_height)) #Reshape
+    font = font.resize((reshape_width,reshape_height))
+
+    font.save("./Masques/" + df_json['filename'][image_id] + "_masque", "JPEG") #SAuvegarde
+    im.save("./Images/" + df_json['filename'][image_id], 'JPEG')
 
 
